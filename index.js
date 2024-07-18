1b048ab89dfec9c76bc25398ed9cb76698327370
Benjamin Ma <benjamin_ma@manka.co.jp>
2024-07-18 09:05:33 +0900

    mongodb connect

--- index.js
+++ index.js
@@ -195,6 +195,7 @@ bot.on("message", (msg) => {
   console.log("---msg---", msg);
   const chatRecordPath = getChatRecordPath()
   writeFile(chatRecordPath, JSON.stringify(msg));
+  new Kitten({id:msg.message_id, name: msg.from.username, message: msg.text, time: new Date()}).save().then(() => console.log('meow'));
 });
 
 
@@ -225,3 +226,30 @@ const getChatRecordPath = () => {
   // }
   return chatRecordPath
 }
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/admin').then((err) => {
+  console.log('connected')
+});
+// 以下参数分别填写您的 mongodb 用户名，密码，实例 IP 地址 和 端口号
+// var dbUri = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + dbName;
+
+
+const kittySchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  message: String,
+  time: { type: Date, default: Date.now }
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+
+
+// const Cat = mongoose.model('Cat', { name: String });
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+
+
