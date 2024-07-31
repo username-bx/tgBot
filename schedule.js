fdbe1451b4fbbff648600b095467d09e1404bbfa
Emma <jackal>
2024-07-31 10:48:54 +0900

    test

--- schedule.js
+++ schedule.js
@@ -2,8 +2,8 @@ const { bot } = require("./telegram");
 const CronJob = require("cron").CronJob;
 const job = new CronJob("*/1 * * * *", () => {
   console.log("每隔1分钟执行一次的定时任务");
-  // bot.sendMessage(6914665681, "欠我3000,记得还钱");
-  bot.sendMessage(6662926132, "欠我3000,记得还钱");
+  bot.sendMessage(6914665681, "欠我3000,记得还钱");
+  // bot.sendMessage(6662926132, "欠我3000,记得还钱");
 });
 // job.start();
 module.exports = {
