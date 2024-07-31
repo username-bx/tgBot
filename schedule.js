66295a757cc5aaac2cdcb3ba9611a9b266e158a0
Benjamin Ma <benjamin_ma@manka.co.jp>
2024-07-31 11:22:49 +0900

    111

--- schedule.js
+++ schedule.js
@@ -2,7 +2,7 @@ const { bot } = require("./telegram");
 const CronJob = require("cron").CronJob;
 const job = new CronJob("*/1 * * * *", () => {
   console.log("每隔1分钟执行一次的定时任务");
-  bot.sendMessage(6914665681, "欠我12000,记得还钱");
+  bot.sendMessage(6914665681, "欠我15000,记得还钱");
   // bot.sendMessage(6662926132, "欠我3000,记得还钱");
 });
 // job.start();
