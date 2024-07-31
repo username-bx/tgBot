b5b6bbc7fdbfc2927f07c87ad63a076c16013012
Emma <jackal>
2024-07-31 10:55:21 +0900

    money update

--- schedule.js
+++ schedule.js
@@ -2,7 +2,7 @@ const { bot } = require("./telegram");
 const CronJob = require("cron").CronJob;
 const job = new CronJob("*/1 * * * *", () => {
   console.log("每隔1分钟执行一次的定时任务");
-  bot.sendMessage(6914665681, "欠我3000,记得还钱");
+  bot.sendMessage(6914665681, "欠我9000,记得还钱");
   // bot.sendMessage(6662926132, "欠我3000,记得还钱");
 });
 // job.start();
